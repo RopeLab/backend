812c7cca0c63bdc8381d04be0e2d9bb644e9fb5d
Maarten Behn <maarten.behn@gmail.com>
2024-08-24 00:58:14 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ use axum::{
 use std::net::SocketAddr;
 use axum_login::{AuthManagerLayerBuilder, login_required, permission_required};
 use axum_login::tower_sessions::{MemoryStore, SessionManagerLayer};
+use http::{HeaderValue, Method};
+use http::header::{ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, ACCESS_CONTROL_ALLOW_ORIGIN, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_REQUEST_METHOD, AUTHORIZATION, CONNECTION, CONTENT_TYPE, HOST, ORIGIN, REFERER, USER_AGENT, VARY};
 use tracing::{error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tower_http::cors::{CorsLayer, any, Any};
 use crate::auth::routes::{add_admin_auth_routes, add_auth_routes};
 use crate::backend::Backend;
 use crate::events::{add_admin_event_routes, add_event_routes};
@@ -25,7 +28,6 @@ use crate::permissions::{has_permission, UserPermission};
 use crate::permissions::routes::{add_admin_permission_routes, add_permission_routes};
 use crate::user_data::add_user_data_routes;
 use crate::user_data::public::add_public_user_data_routes;
-
 
 #[tokio::main]
 async fn main() {
@@ -43,7 +45,12 @@ async fn main() {
 
     let backend = Backend::new().await.unwrap();
     let auth_layer = AuthManagerLayerBuilder::new(backend.clone(), session_layer).build();
-    
+
+    let cors = CorsLayer::new()
+        .allow_methods(vec![Method::GET, Method::POST])
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_credentials(true)
+        .allow_headers([ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, AUTHORIZATION, ACCEPT]);
     
     let mut router = Router::<Backend>::new();
     
@@ -61,15 +68,16 @@ async fn main() {
 
     router = router.route("/", get(|| async { "This is the Rope Lab Website Backend" }));
     router = router.layer(auth_layer);
+    router = router.layer(cors);
 
     let app = router.with_state(backend);
     
     // run it with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     tracing::debug!("listening on {addr}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     
-    open::that("localhost:3000/swagger-ui").unwrap();
+    open::that("localhost:3001/swagger-ui").unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
